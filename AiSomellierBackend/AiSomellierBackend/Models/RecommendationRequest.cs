fff713cac7112aea41aecace0526d87f91f30578
Palmisano, Daniel <Daniel.Palmisano+wkl@wolterskluwer.com>
2026-04-02 09:22:31 +0200

    Add .gitignore

--- AiSomellierBackend/AiSomellierBackend/Models/RecommendationRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace AiSomellierBackend.Models;
-
-public class RecommendationRequest
-{
-    public string MealType { get; set; } = string.Empty;
-    public string Occasion { get; set; } = string.Empty;
-    public string Season { get; set; } = string.Empty;
-}
